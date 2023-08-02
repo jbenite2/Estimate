1bd2fcb80999a7a8274b1b0a6c716bfae23b08e9
Jose <jbenite2@nd.edu>
2023-08-02 11:24:48 -0400

    new states added

--- components/dataInput.js
+++ components/dataInput.js
@@ -2,29 +2,59 @@ import './dataInput.css';
 import React, { useState } from 'react';
 
 const qualityOptions = [20, 40, 60, 80, 100];
-const thicknessOptions = [
-  '', '1/64"', '1/32"', '1/16"', '1/8"',
+const thicknessOptions = ['1/64"', '1/32"', '1/16"', '1/8"',
   '3/16"', '5/16"', '1/4"', '3/8"',
   '1/2"', '3/4"', '1"', '2"'
 ];
-const materialColorOptions = ['', 'Black', 'White', 'Gray', 'Red', 'Blue'];
+const materialColorOptions = ['Mate Black', 'White', 'Gray', 'Red', 'Blue'];
 
-const InputComponent = ({ onSubmit}) => {
-  const [thickness, setThickness] = useState('');
-  const [materialName, setMaterialName] = useState('');
-  const [height, setHeight] = useState('');
-  const [width, setWidth] = useState('');
-  const [materialColor, setMaterialColor] = useState('');
-  const [quality, setQuality] = useState('');
+const InputComponent = ({ onSubmit }) => {
+  const [productName, setProductName] = useState('');
+  const [materialType, setMaterialType] = useState('');
+  const [productThickness, setProductThickness] = useState('');
+  const [productWidth, setProductWidth] = useState('');
+  const [productHeight, setProductHeight] = useState('');
+  const [productColor, setProductColor] = useState('');
+  const [purchasedWidth, setPurchasedWidth] = useState('');
+  const [purchasedHeight, setPurchasedHeight] = useState('');
+  const [purchasedThickness, setPurchasedThickness] = useState('');
+  const [materialColorPurchased, setMaterialColorPurchased] = useState('');
+  const [materialCosts, setMaterialCosts] = useState(0);
+  const [cutQuality, setCutQuality] = useState('');
+  const [estimatedAbrasiveUse1, setEstimatedAbrasiveUse1] = useState('');
+  const [estimatedAbrasiveUse2, setEstimatedAbrasiveUse2] = useState('');
+  const [estimatedCutTime, setEstimatedCutTime] = useState('00:00:00');
+
+  const isValidStopwatchTime = (inputTime) => {
+    // Add validation logic here if needed
+    // For simplicity, we assume any input is valid in this example.
+    return true;
+  };
+
+  const handleStopwatchTimeChange = (e) => {
+    const inputTime = e.target.value;
+    if (isValidStopwatchTime(inputTime)) {
+      setEstimatedCutTime(inputTime);
+    }
+  };
 
   const handleSubmit = () => {
     const formData = {
-      thickness,
-      materialName,
-      height,
-      width,
+      productName,
+      materialType,
+      productThickness,
+      productWidth,
+      productHeight,
       materialColor,
-      quality,
+      purchasedWidth,
+      purchasedHeight,
+      purchasedThickness,
+      materialColorPurchased,
+      materialCosts,
+      cutQuality,
+      estimatedAbrasiveUse1,
+      estimatedAbrasiveUse2,
+      estimatedCutTime,
     };
     console.log(formData); // You can replace this with any other action you want to perform with the data
     onSubmit(formData); // Call the onSubmit prop with the form data
@@ -32,15 +62,33 @@ const InputComponent = ({ onSubmit}) => {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+      <label style={{ marginTop: '50px' }} className="input-label">
+        Product Name:
+        <input
+          className="input-field"
+          type="text"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+        />
+      </label>
       <label className="input-label">
-        Quality:
+        Material Type:
+        <input
+          className="input-field"
+          type="text"
+          value={materialType}
+          onChange={(e) => setMaterialType(e.target.value)}
+        />
+      </label>
+      <label className="input-label">
+        Product Thickness (inches):
         <select
           className="input-field"
-          value={quality}
-          onChange={(e) => setQuality(e.target.value)}
+          value={productThickness}
+          onChange={(e) => setProductThickness(e.target.value)}
         >
           <option value=""> </option>
-          {qualityOptions.map((option) => (
+          {thicknessOptions.map((option) => (
             <option key={option} value={option}>
               {option}
             </option>
@@ -48,40 +96,81 @@ const InputComponent = ({ onSubmit}) => {
         </select>
       </label>
       <label className="input-label">
-        Material Name:
-        <input
-          className="input-field"
-          type="text"
-          value={materialName}
-          onChange={(e) => setMaterialName(e.target.value)}
-        />
-      </label>
-      <label className="input-label">
-        Height (inches):
+        Product Width (inches):
         <input
           className="input-field"
           type="number"
           step="0.01"
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
+          value={productWidth}
+          onChange={(e) => setProductWidth(e.target.value)}
         />
       </label>
       <label className="input-label">
-        Width (inches):
+        Product Height (inches):
         <input
           className="input-field"
           type="number"
           step="0.01"
-          value={width}
-          onChange={(e) => setWidth(e.target.value)}
+          value={productHeight}
+          onChange={(e) => setProductHeight(e.target.value)}
         />
+      </label>
+      <label className="input-label">
+        Product Color:
+        <select
+          className="input-field"
+          value={productColor}
+          onChange={(e) => setProductColor(e.target.value)}
+        >
+          <option value=""> </option>
+          {materialColorOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label style={{ marginTop: '50px' }} className="input-label">
+        Purchased Width (inches):
+        <input
+          className="input-field"
+          type="number"
+          step="0.01"
+          value={purchasedWidth}
+          onChange={(e) => setPurchasedWidth(e.target.value)}
+        />
+      </label>
+      <label className="input-label">
+        Purchased Height (inches):
+        <input
+          className="input-field"
+          type="number"
+          step="0.01"
+          value={purchasedHeight}
+          onChange={(e) => setPurchasedHeight(e.target.value)}
+        />
+      </label>
+      <label className="input-label">
+        Purchased Thickness (inches):
+        <select
+          className="input-field"
+          value={purchasedThickness}
+          onChange={(e) => setPurchasedThickness(e.target.value)}
+        >
+          <option value=""> </option>
+          {thicknessOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
       </label>
       <label className="input-label">
         Material Color:
         <select
           className="input-field"
-          value={materialColor}
-          onChange={(e) => setMaterialColor(e.target.value)}
+          value={materialColorPurchased}
+          onChange={(e) => setMaterialColorPurchased(e.target.value)}
         >
           <option value=""> </option>
           {materialColorOptions.map((option) => (
@@ -92,19 +181,58 @@ const InputComponent = ({ onSubmit}) => {
         </select>
       </label>
       <label className="input-label">
-        Thickness:
+        Material Costs ($):
+        <input
+          className="input-field"
+          type="number"
+          step="0.01"
+          value={materialCosts}
+          onChange={(e) => setMaterialCosts(parseFloat(e.target.value))}
+        />
+      </label>
+      <label style={{ marginTop: '50px' }} className="input-label">
+        Cut Quality:
         <select
           className="input-field"
-          value={thickness}
-          onChange={(e) => setThickness(e.target.value)}
+          value={cutQuality}
+          onChange={(e) => setCutQuality(e.target.value)}
         >
           <option value=""> </option>
-          {thicknessOptions.map((option) => (
+          {qualityOptions.map((option) => (
             <option key={option} value={option}>
               {option}
             </option>
           ))}
         </select>
+      </label>
+      <label className="input-label">
+        Estimated Abrasive Use (lbs):
+        <input
+          className="input-field"
+          type="number"
+          step="0.01"
+          value={estimatedAbrasiveUse1}
+          onChange={(e) => setEstimatedAbrasiveUse1(e.target.value)}
+        />
+      </label>
+      <label className="input-label">
+        Estimated Abrasive Use (lbs):
+        <input
+          className="input-field"
+          type="number"
+          step="0.01"
+          value={estimatedAbrasiveUse2}
+          onChange={(e) => setEstimatedAbrasiveUse2(e.target.value)}
+        />
+      </label>
+      <label className="input-label">
+        Estimated Cut Time:
+        <input
+          className="input-field"
+          type="text"
+          value={estimatedCutTime}
+          onChange={handleStopwatchTimeChange}
+        />
       </label>
       <button
         style={{
--- pages/all.js
+++ pages/all.js
@@ -4,41 +4,9 @@ import { Dropdown, Input, Reveal } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import { Comforter, Maiden_Orange } from 'next/font/google';
 import Header from '../components/header'
-import productData from '../components/productData'
 
 
 export default function Home() {
-  // const [selectedImage, setSelectedImage] = useState('p1');
-
-  // const handleImageClick = (imageId) => {
-  //   setSelectedImage(imageId);
-  // };
-
-
-  // const colorOptions = [
-  //   { key: 'red', value: 'red', text: 'Red' },
-  //   { key: 'blue', value: 'blue', text: 'Blue' },
-  //   { key: 'green', value: 'green', text: 'Green' },
-  //   { key: 'yellow', value: 'yellow', text: 'Yellow' },
-  // ];
-
-  // const thicknessOptions = [
-  //   { key: '1/16', value: '1/16', text: '1/16' },
-  //   { key: '1/8', value: '1/8', text: '1/8' },
-  //   { key: '1/4', value: '1/4', text: '1/4' },
-  //   { key: '1/2', value: '1/2', text: '1/2' },
-  //   { key: '1', value: '1', text: '1' },
-  // ];
-
-  // const qualityOptions = [
-  //   { key: '20', value: '20', text: '20' },
-  //   { key: '40', value: '40', text: '40' },
-  //   { key: '60', value: '60', text: '60' },
-  //   { key: '80', value: '80', text: '80' },
-  //   { key: '100', value: '100', text: '100' },
-  // ];
-  
-
   return (
     <>
       <Header/>
