cf7b60434b0b2b64133eb80a54a37e4b4b0e4601
Jose <jbenite2@nd.edu>
2023-12-21 19:09:23 -0400

    wip

--- pages/categorias.js
+++ pages/categorias.js
@@ -5,33 +5,14 @@ import './categorias.scss';
 import { useRouter } from 'next/router';
 
 export default function Categorias() {
-  const [directories, setDirectories] = useState([]);
   const router = useRouter();
 
-  useEffect(() => {
-    async function fetchDirectories() {
-      try {
-        const response = await fetch('/api/categorias');
-        if (response.ok) {
-          const data = await response.json();
-          setDirectories(data.directories);
-        } else {
-          throw new Error('Failed to fetch data');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-
-    fetchDirectories();
-
-  }, []);
 
   return (
 	<div>
 		<Header />
 		<div class="btnsDiv">
-			<button type="button" className="btn btn--green" onClick={() => router.push('/collections/barandas')}>Barandas</button>
+		  <button type="button" className="btn btn--green" onClick={() => router.push('/collections/barandas')}>Barandas</button>
 		  <button type="button" className="btn btn--red">Escaleras</button>
 		  <button type="button" className="btn btn--blue">Industriales</button>
 		  <button type="button" className="btn btn--green">Portones</button>
