63d4686af3741a453378e6d52b92cdb226aa62f4
José Benítez <josebenitez@jbsmbp.local>
2024-12-04 15:17:33 -0600

    Deleted a bunch of pictures and changed product image width

--- components/header.js
+++ components/header.js
@@ -1,24 +1,42 @@
 import './header.css';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-export default function Header(){
+export default function Header() {
+    const router = useRouter();
+
+    const navigateTo = (path) => {
+        router.push(path);
+    };
+
     return (
         <div className="header">
-			<a><img src="/BM-Logo.png" className="logo" alt="Logo" /></a>
+            <a>
+                <img src="/BM-Logo.png" className="logo" alt="Logo" />
+            </a>
             <div className="button-container">
-				<button className="button"> 
-					<img className='book-icon' src='/book-icon.png'></img>
-					<Link className='Link' href='/categorias'>Categories</Link>
-				</button>
-				<button className="button">
-					<img className='add-icon' src='/add_icon.png'></img>
-					<Link className='Link' href='/quote'>Template</Link>
-				</button>
-                <button className="button">
-					<img className='user-icon' src='/user-icon.png'></img>
-					<Link className='Link' href='/all'>All Patterns</Link>
-				</button>
+                <button 
+                    className="button" 
+                    onClick={() => navigateTo('/categorias')}
+                >
+                    <img className="book-icon" src="/book-icon.png" alt="Categories Icon" />
+                    <span>Categories</span>
+                </button>
+                <button 
+                    className="button" 
+                    onClick={() => navigateTo('/quote')}
+                >
+                    <img className="add-icon" src="/add_icon.png" alt="Template Icon" />
+                    <span>Template</span>
+                </button>
+                <button 
+                    className="button" 
+                    onClick={() => navigateTo('/all')}
+                >
+                    <img className="user-icon" src="/user-icon.png" alt="All Patterns Icon" />
+                    <span>All Patterns</span>
+                </button>
             </div>
-		</div>
-	)
+        </div>
+    );
 }
+
